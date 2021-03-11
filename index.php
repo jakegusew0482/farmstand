e09ef731d0d6e6eb8ad52be9769593269f62cfa4
Maky <franklinjaradev@gmail.com>
2021-03-10 21:21:12 -0500

    Added map implementation

--- index.php
+++ index.php
@@ -4,32 +4,6 @@
 	<div class="row">
 		<div class="side">
 			<h2>Support Your Local Farm Stand</h2>
-			<div id="GeoLocation">
-
-				<p>Find a local Stand.</p>
-
-				<button onclick="getLocation()">Find a local Stand</button>
-				<p id="demo"></p>
-
-				<script>
-					var x = document.getElementById("demo");
-
-					function getLocation() {
-						if (navigator.geolocation) {
-							navigator.geolocation.getCurrentPosition(showPosition);
-						} else {
-							x.innerHTML = "Geolocation is not supported by this browser.";
-						}
-					}
-
-					function showPosition(position) {
-						x.innerHTML = "Latitude: " + position.coords.latitude +
-							"<br>Longitude: " + position.coords.longitude;
-					}
-				</script>
-
-			</div>
-
 			<h3>Search by Type of goods</h3>
 			<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Type of stand" />
 
@@ -38,21 +12,62 @@
 		<!--Main Page-->
 		<div class="main">
 			<h4>Local Farm Stand News</h4>
-
 			<p>login status:</p>
 			<p id='login'></p>
+			<div id="myMap" style="height: 400px"></div>
+			<script>
+				let myMap = L.map("myMap");
+
+				// Create map - use mapBox
+				L.tileLayer(
+					"https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+						attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+						maxZoom: 18,
+						id: "mapbox/streets-v11",
+						tileSize: 512,
+						zoomOffset: -1,
+						accessToken: "pk.eyJ1IjoibWFreWZqIiwiYSI6ImNrbHJib21oNjAyZnUyb2xkZnJvcG5kZW8ifQ.EoSKv-aBqUhwqjmcH6fERg",
+					}
+				).addTo(myMap);
+
+				myMap.locate({
+					setView: true,
+					maxZoom: 16
+				});
+
+				function onLocationFound(e) {
+					let radius = e.accuracy;
+
+					L.marker(e.latlng).addTo(myMap);
+
+					L.circle(e.latlng, radius).addTo(myMap);
+				}
+
+				myMap.on('locationfound', onLocationFound);
+
+				function onLocationError(e) {
+					alert(e.message);
+				}
+
+				myMap.on('locationerror', onLocationError);
+
+				myMap.locate({
+					setView: true,
+					maxZoom: 16
+				});
+			</script>
 		</div>
 	</div>
 
 	<div class="footer">
 		<h5>To Reach us, send us an Email: www.somekindofemail@something.com</h5>
 	</div>
-</body>
+	</body>
 
-</html>
+	</html>
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script language='Javascript' type='text/javascript'>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script language='Javascript' type='text/javascript'>
 
 
-</script>
+	</script>
--- navbar.php
+++ navbar.php
@@ -1,45 +1,44 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-        <title>Farm Stand</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="styles/styles.css" media="screen" />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
-        <script src="scripts/indexJS.js"></script>
-        <!-- script src="scripts/SearchBarScript.js"></script>-->
+	<title>Farm Stand</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="styles/styles.css" media="screen" />
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+	<script src="scripts/indexJS.js"></script>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+	<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+	<!-- script src="scripts/SearchBarScript.js"></script>-->
 </head>
 
 <body>
-        <div class="header">
-                <h1>Add Health Choices At Your Local Stand</h1>
-                <p>Find Local Stand Near You</p>
-        </div>
+	<div class="header">
+		<h1>Add Health Choices At Your Local Stand</h1>
+		<p>Find Local Stand Near You</p>
+	</div>
 
 
 
-<div class="navbar">
-      <a href="index.php">Home</a>
-	<a href="farmstands.php">Farm Stands</a>
-        <?php
-                include('config.php');
-                if(isset($_SESSION['username'])) {
-                        echo"<a href='logout.php'>Log Out</a>";
-                }
-                else {
-                        echo "<a href='loginPage.php'>Log In</a>";
-                }
-        ?>
-
-      <a href="Feedback.php" class="right">Feedback</a>
-      <a href="ContactUs.php" class="right">Contact Us</a>
-
-	<?php	
-		if(!(isset($_SESSION['username']))) {
-		     echo"<a href='createAccount.php' class='right'>Create Account</a>";
+	<div class="navbar">
+		<a href="index.php">Home</a>
+		<a href="farmstands.php">Farm Stands</a>
+		<?php
+		include('config.php');
+		if (isset($_SESSION['username'])) {
+			echo "<a href='logout.php'>Log Out</a>";
+		} else {
+			echo "<a href='loginPage.php'>Log In</a>";
 		}
-	?>
-    </div>
+		?>
 
+		<a href="Feedback.php" class="right">Feedback</a>
+		<a href="ContactUs.php" class="right">Contact Us</a>
+
+		<?php
+		if (!(isset($_SESSION['username']))) {
+			echo "<a href='createAccount.php' class='right'>Create Account</a>";
+		}
+		?>
+	</div>
