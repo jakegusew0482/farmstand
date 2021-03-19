21c6247f2bee4c1ac344eb4716b1425a0d5295c3
Maky <franklinjaradev@gmail.com>
2021-03-19 00:12:01 -0400

    Cleaned files

--- NewMarketPlace.php
+++ NewMarketPlace.php
@@ -1,25 +1,25 @@
 <?php include('navbar.php'); ?>
-	<div class="main">
-		<div id= "marketPlaceContainer">
-			<div id="MarketNodeContainer">
-			
-				<!--Main Container-->
-				<!--Text input from previous pages should be locked in div id containers-->
-				<div class="UserStandContainer">
-					
-					<?php 
-					include('config.php');
-					$name = $_SESSION['farmstand_name'];
-					$desc = $_SESSION['farmstand_description'];
-					$address = $_SESSION['farmstand_address'];
-					$imgsrc = $_SESSION['farmstand_image'];
-					echo"<div id = 'ImageContainer'>
+<div class="main">
+	<div id="marketPlaceContainer">
+		<div id="MarketNodeContainer">
+
+			<!--Main Container-->
+			<!--Text input from previous pages should be locked in div id containers-->
+			<div class="UserStandContainer">
+
+				<?php
+				include('config.php');
+				$name = $_SESSION['farmstand_name'];
+				$desc = $_SESSION['farmstand_description'];
+				$address = $_SESSION['farmstand_address'];
+				$imgsrc = $_SESSION['farmstand_image'];
+				echo "<div id = 'ImageContainer'>
 					<img src='$imgsrc' id ='FarmStandImage' style='width:100%'> 
 					</div>
 					<div id = 'StandNameContainer'>
 
 					";
-					echo "<h1 id = 'StandNamePass'> $name  </h1> 
+				echo "<h1 id = 'StandNamePass'> $name  </h1> 
 					</div>
 					<div id = 'DescriptionContainer'>
 					<p id ='StandDescriptionPass'>$desc</p>
@@ -27,33 +27,33 @@
 					<div id = 'AddressContainer'>
 					<p id = 'StandAddressPass'>$address</p>
 					</div>"; ?>
-					
-					
-					
-					
-					
-					
-					<p>
-						<button onclick="document.getElementById('AddItemsButton').style.display='block'">Add Items</button>
-					</p>
-	</div>
-		</div>
-			</div>
-				</div>		
-				
-				
-	<!--
-		Modal Window for adding inventory
-	-->			
-	
-	
 
-	<div id="AddItemsButton" class="modal">
-		<span onclick="document.getElementById('AddItemsButton').style.display='none'" class="close" title="Close Modal"></span>
-		<div class="modal-content">
+
+
+
+
+
+				<p>
+					<button onclick="document.getElementById('AddItemsButton').style.display='block'">Add Items</button>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!--
+		Modal Window for adding inventory
+	-->
+
+
+
+<div id="AddItemsButton" class="modal">
+	<span onclick="document.getElementById('AddItemsButton').style.display='none'" class="close" title="Close Modal"></span>
+	<div class="modal-content">
 		<div class="container">
 			<h1>Add new inventory</h1>
-			
+
 			<div class="UserInventoryAdd">
 				<form id='productform' name='productform' method='post' action='addProduct.php' enctype='multipart/form-data'>
 
@@ -62,123 +62,75 @@
 					<label for="desc">Item Description</label><br>
 					<input type="text" id="desc" name="desc" placeholder="Item Description"><br>
 
-						<p>Inventory Images</p>
-						<div class="panel">
-							
-							<label for="files">Select file:</label>
-							<input id="uploadImage" type="file" accept="image/*" name="image"/><br><br>
-							
-						</div>
-						<div id = "QTYandPrice">							
-								<label for="quantity">Price:</label>
-								<input type="number" id="price" name="price" >
-						</div>
-						<p id='test'>test</p>
-							<?php $id = $_SESSION['farm_id'];
-						echo "<input style='display:none;' id='id' name = 'id' value='$id'>"; ?>
-						</div>
-						<br>
-					<br>
-					
+					<p>Inventory Images</p>
+					<div class="panel">
 
-					<input class="btn btn-success" type="submit" value="Add Item">
+						<label for="files">Select file:</label>
+						<input id="uploadImage" type="file" accept="image/*" name="image" /><br><br>
 
-					</form>
-
-
-					<div class="clear">
-						<button type="button" onclick="document.getElementById('AddItemsButton').style.display='none'" class="cancelbtn">Cancel</button>
 					</div>
+					<div id="QTYandPrice">
+						<label for="quantity">Price:</label>
+						<input type="number" id="price" name="price">
+					</div>
+					<p id='test'>test</p>
+					<?php $id = $_SESSION['farm_id'];
+					echo "<input style='display:none;' id='id' name = 'id' value='$id'>"; ?>
 			</div>
-
-	</body>
-</html>
-
-					
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>					
-<script>
+			<br>
+			<br>
 
 
-	$(document).ready(function(e) {
-	 $("#productform").on('submit',(function(e) {
-	  e.preventDefault();
-					
-		$.ajax({
-         		url: "addProduct.php",
-  			type: "POST",
-   			data:  new FormData(this),
-   			contentType: false,
-       			cache: false,
-  			processData:false,
-   			success: function(data) {
-     				$("#productform")[0].reset(); 
-			}
-    		});
- 	}));
-});
+			<input class="btn btn-success" type="submit" value="Add Item">
+
+			</form>
 
 
+			<div class="clear">
+				<button type="button" onclick="document.getElementById('AddItemsButton').style.display='none'" class="cancelbtn">Cancel</button>
+			</div>
+		</div>
 
-						function CreateNewLine()
-						{
-							var divElement = document.createElement("DIV");
-							var t = document.createTextNode("This is a div element.");
-							divElement.setAttribute("style", "background-color: pink;");
-							divElement.appendChild(t);
-							document.body.appendChild(divElement);
-								
+		</body>
 
+		</html>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script>
+			$(document).ready(function(e) {
+				$("#productform").on('submit', (function(e) {
+					e.preventDefault();
+
+					$.ajax({
+						url: "addProduct.php",
+						type: "POST",
+						data: new FormData(this),
+						contentType: false,
+						cache: false,
+						processData: false,
+						success: function(data) {
+							$("#productform")[0].reset();
 						}
-					</script>
+					});
+				}));
+			});
 
 
 
-				</form> 
-
-			</div>
-
-
-
-
-
+			function CreateNewLine() {
+				var divElement = document.createElement("DIV");
+				var t = document.createTextNode("This is a div element.");
+				divElement.setAttribute("style", "background-color: pink;");
+				divElement.appendChild(t);
+				document.body.appendChild(divElement);
 
 
-
-
+			}
+		</script>
 
 
 
+		</form>
 
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	</div>
--- testData/loadMockData.php
+++ testData/loadMockData.php
@@ -12,7 +12,11 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
+// works on localhost
 $sql = "LOAD DATA INFILE '/opt/lampp/htdocs/farmstand/testData/mockData.csv' INTO TABLE farmstand FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS";
+
+// works on server
+// $sql = "LOAD DATA INFILE '/opt/lampp/htdocs/farmstand/testData/mockData.csv' INTO TABLE farmstand FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS";
 
 if ($conn->query($sql) === TRUE) {
 	echo "Records Loaded successfully";
