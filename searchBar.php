efd6938f0db087339786f9d9ecf5c45b8872a25c
Jake <jake.gusewnpt@gmail.com>
2021-03-24 21:45:49 -0400

    search function

--- searchBar.php
+++ searchBar.php
@@ -3,9 +3,28 @@
         <h3>Search For Farmstands!</h3>
 
 	<form id='searchForm'>
-        <input type="text" id="search" placeholder="Enter Search Term"/>
+	<table style="width:100%;"><tr><td>
 
-	<button type='submit' id='submitbutton'>Search</button>
+	<select id="searchType" style="width:30%;">
+		<option value="product">Product</option>
+		<option value="title">Farmstand Title</option>
+		<option value="zipcode">Zipcode</option>
+	</select>
+
+
+	<input type="text" id="search" placeholder="Enter Search Term" style="width:65%;"/>
+
+
+    <!--    <input type="text" id="search" placeholder="Enter Search Term"/>
+	</td></tr><tr>
+	<label><input type="radio" name="searchOption" id="searchOption">Zipcode</label>
+	<label><input type="radio" name="searchOption" id="searchOption">Farmstand Name</label>
+	<label><input type="radio" name="searchOption" id="searchOption">Product</label> -->
+
+
+	</tr></table>
+	<button type='submit' id='submitbutton' style="width:95%;">Search</button>
+
 	</form>
 
 	<div id="results" style="overflow:scroll; height:50%;">
@@ -21,13 +40,14 @@
 					e.preventDefault();
 					
 					var search = document.getElementById('search').value;
+					var searchtype = document.getElementById('searchType').value;
 					
 				if(search != "") {
 
 					$.ajax({
 						url: "searchResult.php",
 						type: "POST",
-						data: {search:search},
+						data: {search:search,searchtype:searchtype},
 						dataType: "html",
 						success: function(data) {
 						var result = $('<div />').append(data).find('#result').html();
--- searchResult.php
+++ searchResult.php
@@ -3,23 +3,25 @@
 	include('mysqli_connect.php');
 
 	if(isset($_POST['search'])) $searchterm = $_POST['search']; else $searchterm = NULL;
+	if(isset($_POST['searchtype'])) $searchtype = $_POST['searchtype']; else $searchtype = NULL;
 
-	$query = "select f.title, f.description, f.coverimage, p.name from farmstand as f inner join product as p on p.farmstand_id = f.farmstand_id where p.name LIKE '%$searchterm%'";
+	if($searchtype == "product") {
+		$query = "select f.title, f.description, f.coverimage, p.name from farmstand as f inner join product as p on p.farmstand_id = f.farmstand_id where p.name LIKE '%$searchterm%'";
+
+	} elseif($searchtype == "title") {
+		$query = "select title, description, coverimage from farmstand where title like '%$searchterm%'";
+	} elseif($searchtype == "zipcode") {
+		$query = "select * from farmstand where zip_code like '%$searchterm%'";
+	}
 
 	$result = mysqli_query($connect, $query);
-
 	echo "<div id='result'>";
-
 	echo "<table><tr>";
-
-	$row = mysqli_fetch_assoc($result);
-
 
 	while($row = mysqli_fetch_assoc($result)) {
 		$title = $row['title'];
 		$desc = $row['description'];
 		$img = $row['coverimage'];
-		
 		
 		echo "<td><h2>$title</h2><h3>$desc</h3><h3>$img</h3></td></tr><tr>";
 	}
