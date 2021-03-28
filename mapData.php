583483089a0c30e662a37ad49dc70945c84c06a3
Maky <franklinjaradev@gmail.com>
2021-03-28 15:05:22 -0400

    Adding better data for the map

--- mapData.php
+++ mapData.php
@@ -1,16 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 // connects to localhost - uncomment to test it in your localhost
-$conn = mysqli_connect("localhost", "root", "", "farm_db");
+/* $conn = mysqli_connect("localhost", "root", "", "farm_db"); */
 
 // server host
-/* $conn = mysqli_connect("localhost", "farm_user", "password", "farm_db"); */
+$conn = mysqli_connect("localhost", "farm_user", "password", "farm_db");
 
 // local host
-$result = mysqli_query($conn, "SELECT title ,address, city, state, zipCode FROM farmstand");
+/* $result = mysqli_query($conn, "SELECT title ,address, city, state, zipCode FROM farmstand"); */
 
 // server data
-/* $result = mysqli_query($conn, "SELECT title ,address, city, state, zip_code FROM farmstand"); */
+$result = mysqli_query($conn, "SELECT title ,address, city, state, zip_code FROM farmstand");
 
 // empty array to store query $result 
 $data = array();
