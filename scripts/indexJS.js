f84752d88f4c7980530a925436bc646741aff584
Nostrama <79551875+Nostrama@users.noreply.github.com>
2021-04-02 20:02:18 -0400

    Update indexJS.js

--- scripts/indexJS.js
+++ scripts/indexJS.js
@@ -8,6 +8,21 @@ function getLocation() {
   }
 }
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 /* How to get longitude and latitude Google Maps
 function showPosition(position) {
   let lat = position.coords.latitude;
