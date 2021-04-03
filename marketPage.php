2aaf270979ace992bb220fea2cbf0dad458b8e7e
Nostrama <79551875+Nostrama@users.noreply.github.com>
2021-04-02 20:05:06 -0400

    Update marketPage.php

--- marketPage.php
+++ marketPage.php
@@ -127,16 +127,44 @@
 
 
 		<!--UserReviewsArea-->
-		<div id = "UserReviewContainer">
-			<div id = "UserReviewTitle"></div>
-			<div id = "UserReviewContent">
-				
+		<div id = "SideShoppingContainer">
+
+	<div id = "ReservationInfoContainer">
+		<form action="/action_page.php">
+          	<h3>Item Reservation</h3>
+		<p>Please fill out the following information</p>
+            	<label for="fname">Name</label>
+            	<input type="text" id="fname" name="firstname" placeholder="First and Last Name">
+            	<label for="Phone Number"><i id="PhoneNumber"></i> Phone Number</label>
+            	<input type="text" id="PhoneNumber" name="Phone Number" placeholder="555-555-5555">  
+      		</form>
+	<label for="PaymentReservation">Payment Type?</label>
+
+<select name="PaymentTime" id="PaymentTime">
+  <option value="Arrival">Pay at the stand</option>
+  <option value="">Pay Now using online processing</option>
+</select>
 
 
+	</div>
 
-			</div>
-		</div>
-	
+
+	<div id = "InventoryReservationContainer">
+
+		<h4>Reservation Cart </h4><span class="price" style="color:black"><i class="fa fa-shopping-cart"></i></span></h4>
+      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
+      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
+      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
+      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+      <hr>
+      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+	</div>
+
+	<div id = "ConfirmReservationButton">
+		<input type="submit" value="Confirm Reservation" class="btn">
+	</div>
+</div>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
