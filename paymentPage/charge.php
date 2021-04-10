6e5accfc62cc9e5f5489dbf6701e94d64b2edde8
Maky <franklinjaradev@gmail.com>
2021-04-09 20:20:58 -0400

    Full example of integration for one time payment

--- paymentPage/charge.php
+++ paymentPage/charge.php
@@ -2,18 +2,14 @@
 
 require("./vendor/autoload.php");
 
-/* \Stripe\Stripe::setApiKey("sk_test_51IdNfnKQJVPZ85OfQtDM8XNNiAbL6HZmVNKPrCzJ5a3OcLxrM9x2nNLoa8smcLKZCu8Qm32hKXJshSPNCkBj4DiQ00jwwDTuPv"); */
-
-/* echo \Stripe\Customer::all(); */
-
 $stripe = new \Stripe\StripeClient(
 	'sk_test_51IdNfnKQJVPZ85OfQtDM8XNNiAbL6HZmVNKPrCzJ5a3OcLxrM9x2nNLoa8smcLKZCu8Qm32hKXJshSPNCkBj4DiQ00jwwDTuPv'
 );
-
-$customer = $stripe->customers->create([
-	'description' => 'example customer',
-	'email' => 'email@example.com',
-	'payment_method' => 'pm_card_visa',
+$stripe->prices->create([
+	'unit_amount' => 2000,
+	'currency' => 'usd',
+	'recurring' => ['interval' => 'month'],
+	'product' => 'prod_JH03UNmaO3VFgf',
 ]);
 
-echo $customer;
+echo $stripe;
